59b37ee9114b2ea28979d342064f92db036a50cc
DaveRomel <davedelcastillo14@gmail.com>
2025-05-15 10:59:42 -0600

    configurracion para el .env

--- config/db.py
+++ config/db.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine, MetaData
+import os
+from dotenv import load_dotenv
 
-engine = create_engine("mysql+pymysql://root:@localhost:3306/storedb2")
-
+load_dotenv()
+#engine = create_engine("mysql+pymysql://root:@localhost:3306/storedb2")
+engine = create_engine("mysql+pymysql://" + os.getenv("USER") + ":@" + os.getenv("DATABASE_HOST") + ":" + os.getenv("DATABASE_PORT") + "/" + os.getenv("DATABASE_NAME"))
 meta_data = MetaData()
